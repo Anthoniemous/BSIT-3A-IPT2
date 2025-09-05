22a011ba6a7ae0bfcd8238fc4048f53ed75b0643
princez01 <bejoc070121@gmail.com>
2025-09-05 21:53:04 +0800

    Login,Register and verify

--- Lagmay-Laravelll/resources/views/layouts/navigation.blade.php
+++ Lagmay-Laravelll/resources/views/layouts/navigation.blade.php
@@ -35,10 +35,7 @@
                         </x-slot>
 
                         <x-slot name="content">
-                            <x-dropdown-link :href="route('profile.edit')">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
-
+                            {{-- Removed Profile link to avoid missing route error --}}
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
@@ -89,9 +86,7 @@
                 </div>
 
                 <div class="mt-3 space-y-1">
-                    <x-responsive-nav-link :href="route('profile.edit')">
-                        {{ __('Profile') }}
-                    </x-responsive-nav-link>
+                    {{-- Removed Profile link to avoid missing route error --}}
 
                     <!-- Authentication -->
                     <form method="POST" action="{{ route('logout') }}">
