6bf81b11590adfe25d2f30defa82378a99c7afd4
cal1mWell <wellalynbaurosanico@gmail.com>
2025-09-11 14:04:02 +0800

    Setup Login With Google

--- barro-laravel-web/resources/views/auth/login.blade.php
+++ barro-laravel-web/resources/views/auth/login.blade.php
@@ -68,11 +68,31 @@
 
             <!-- Footer Links -->
             <div class="mt-6 text-center text-sm text-gray-600">
-                {{ __("Don't have an account?") }}
-                <a href="{{ route('register') }}" 
-                   class="text-orange-500 hover:text-orange-600 font-medium transition">
-                    {{ __('Sign up ') }}
-                </a>
-            </div>
+    {{ __("Don't have an account?") }}
+    <a href="{{ route('register') }}" 
+       class="text-orange-500 hover:text-orange-600 font-medium transition">
+        {{ __('Sign up ') }}
+    </a>
+
+    <!-- Divider -->
+    <div class="flex items-center my-4">
+        <hr class="flex-grow border-gray-300">
+        <span class="mx-2 text-gray-500 text-xs">OR</span>
+        <hr class="flex-grow border-gray-300">
+    </div>
+
+    <!-- Google Login Button -->
+    <a href="{{ url('/auth/google/redirect') }}" 
+       class="w-full inline-flex items-center justify-center px-4 py-2 bg-red-500 hover:bg-red-600 text-white font-medium rounded-lg shadow-md transition">
+        <svg class="w-5 h-5 mr-2" viewBox="0 0 533.5 544.3">
+            <path fill="#4285F4" d="M533.5 278.4c0-17.4-1.6-34-4.6-50.2H272.1v95.3h147.5c-6.3 33.9-25.4 62.6-54.1 81.8l87.2 67.8c51-47 80.8-116.3 80.8-194.7z"/>
+            <path fill="#34A853" d="M272.1 544.3c73.4 0 135-24.3 179.9-66.1l-87.2-67.8c-24.2 16.3-55.2 26-92.7 26-71 0-131.3-47.9-152.9-112.1l-90.2 69.6c43.9 87.3 134 150.4 243.1 150.4z"/>
+            <path fill="#FBBC05" d="M119.2 324.2c-10.3-30-10.3-62.5 0-92.5l-90.2-69.6c-39.4 77.6-39.4 168.2 0 245.8l90.2-69.7z"/>
+            <path fill="#EA4335" d="M272.1 107.7c38.9-.6 75.7 13.7 103.8 39.9l77.5-77.5C406.9 24.2 344.9 0 272.1 0 163 0 72.9 63.1 29 150.4l90.2 69.6c21.6-64.2 81.9-112.3 152.9-112.3z"/>
+        </svg>
+        Login with Google
+    </a>
+</div>
+
         </div>
 </x-guest-layout>
